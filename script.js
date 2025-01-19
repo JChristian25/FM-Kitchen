df100f77da0ef0e432e15688af663a0f1e2a6fa6
JChristian25 <jcl.void@yahoo.com>
2025-01-19 09:55:55 +0800

    sum tweaks

--- script.js
+++ script.js
@@ -4,25 +4,39 @@ const navbar = document.getElementById("navbar");
 // Add a scroll event listener
 window.addEventListener("scroll", () => {
   // Check if the page has been scrolled down
-  if (window.scrollY > 50) {
-    navbar.classList.add("shrink");
-  } else {
-    navbar.classList.remove("shrink");
+  if (window.innerWidth > 768) {
+    if (window.scrollY > 65) {
+      navbar.classList.add("shrink");
+      navbar.classList.remove("full");
+    } else {
+      navbar.classList.remove("shrink");
+      navbar.classList.add("full");
+    }
   }
+
+  let back2top = document.getElementById("back-to-top");
+  if (window.scrollY > 150) {
+    back2top.classList.add("active");
+  } else {
+    back2top.classList.remove("active");
+  }
+
 });
 
 function toggleCarousel(n) {
-  let imgs = document.getElementsByClassName("carousel-slides");
+  let slides = document.getElementsByClassName("carousel-slides");
   let controls = document.getElementsByClassName("carousel-control");
 
-  for (let i = 0; i < imgs.length; i++) {
-    imgs[i].classList.remove("active");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
     controls[i].classList.remove("active");
   }
-  if (n >= 0 && n < imgs.length) {
-    imgs[n].classList.add("active");
+  if (n >= 0 && n < slides.length) {
+    slides[n].classList.add("active");
     controls[n].classList.add("active");
   }
 }
 
-console.log("Hello world!")
+function backtop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
